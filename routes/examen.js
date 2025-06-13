daa8eeb58b12a18e9424d3068104b107a0cfed09
Ernesto <ernesto7816@gmail.com>
2025-06-13 17:28:58 -0500

    examen-rol

--- routes/examen.js
+++ routes/examen.js
@@ -8,7 +8,7 @@ router.get("/all", listado);
 router.get("/all-adm", listarTodo);
 router.get("/all-audit", listarAuditoria);
 router.get("/notas-alum/:id", listarNotasAlumno);
-router.post('/multiple',checkAuth,checkRol('administrador'),crearMultiples);
+router.post('/multiple',checkAuth,checkRol('profesor'),crearMultiples);
 router.delete("/delete/:id",checkAuth,checkRol('profesor'),eliminar)
 router.put("/update/:id",checkAuth,checkRol('profesor'),actualizar)
 router.post("/create", checkAuth,checkRol('profesor'),insertar);
