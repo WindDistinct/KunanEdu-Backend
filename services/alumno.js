777d0a19f0414487982aea35f2b793e9be9f5957
Ernesto <ernesto7816@gmail.com>
2025-06-27 11:55:31 -0500

    alumno-service-1

--- services/alumno.js
+++ services/alumno.js
@@ -6,7 +6,7 @@ async function registrarAuditoriaAlumno({
   nombre_anterior, nombre_nuevo,
   apellido_paterno_anterior, apellido_paterno_nuevo,
   apellido_materno_anterior, apellido_materno_nuevo,
-  dni_anterior, dni_nuevo,
+  numero_documento_anterior, numero_documento_nuevo,
   direccion_anterior, direccion_nuevo,
   telefono_anterior, telefono_nuevo,
   fecha_nacimiento_anterior, fecha_nacimiento_nuevo,
@@ -21,7 +21,7 @@ async function registrarAuditoriaAlumno({
       nombre_anterior, nombre_nuevo,
       apellido_paterno_anterior, apellido_paterno_nuevo,
       apellido_materno_anterior, apellido_materno_nuevo,
-      dni_anterior, dni_nuevo,
+      numero_documento_anterior, numero_documento_nuevo,
       direccion_anterior, direccion_nuevo,
       telefono_anterior, telefono_nuevo,
       fecha_nacimiento_anterior, fecha_nacimiento_nuevo,
@@ -39,7 +39,7 @@ async function registrarAuditoriaAlumno({
     nombre_anterior, nombre_nuevo,
     apellido_paterno_anterior, apellido_paterno_nuevo,
     apellido_materno_anterior, apellido_materno_nuevo,
-    dni_anterior, dni_nuevo,
+    numero_documento_anterior, numero_documento_nuevo,
     direccion_anterior, direccion_nuevo,
     telefono_anterior, telefono_nuevo,
     fecha_nacimiento_anterior, fecha_nacimiento_nuevo,
@@ -60,15 +60,15 @@ async function registrarAuditoriaAlumno({
 async function insertarAlumno(datos, usuarioModificador) {
   const {
     nombre, apellido_paterno, apellido_materno,
-    dni, direccion, telefono, fecha_nacimiento
+    tipo_documento,numero_documento, direccion, telefono, fecha_nacimiento
   } = datos;
 
   const sqlInsert = `
     INSERT INTO tb_alumno (
       nombre, apellido_paterno, apellido_materno,
-      dni, direccion, telefono, fecha_nacimiento, estado
+      tipo_documento,numero_documento, direccion, telefono, fecha_nacimiento, estado
     ) VALUES (
-      $1, $2, $3, $4, $5, $6, $7, true
+      $1, $2, $3, $4, $5, $6, $7, $8, true
     )
     RETURNING id_alumno
   `;
@@ -76,7 +76,7 @@ async function insertarAlumno(datos, usuarioModificador) {
   try {
     const result = await pool.query(sqlInsert, [
       nombre, apellido_paterno, apellido_materno,
-      dni, direccion, telefono, fecha_nacimiento
+      tipo_documento,numero_documento, direccion, telefono, fecha_nacimiento
     ]);
     const id_alumno = result.rows[0].id_alumno;
 
@@ -88,8 +88,8 @@ async function insertarAlumno(datos, usuarioModificador) {
       apellido_paterno_nuevo: apellido_paterno,
       apellido_materno_anterior: null,
       apellido_materno_nuevo: apellido_materno,
-      dni_anterior: null,
-      dni_nuevo: dni,
+      numero_documento_anterior: null,
+      numero_documento_nuevo: numero_documento,
       direccion_anterior: null,
       direccion_nuevo: direccion,
       telefono_anterior: null,
@@ -145,7 +145,7 @@ async function obtenerAlumnosAula(aula,cursoseccion) {
 async function obtenerAlumnosPorPeriodo(idPeriodo) {
   const sql = `
     SELECT DISTINCT a.id_alumno, a.nombre, a.apellido_paterno, a.apellido_materno,
-           a.dni, a.direccion, a.telefono, a.fecha_nacimiento, a.estado
+           a.numero_documento, a.direccion, a.telefono, a.fecha_nacimiento, a.estado
     FROM tb_alumno a
     JOIN tb_matricula m ON a.id_alumno = m.alumno
     JOIN tb_seccion s ON m.seccion = s.id_seccion
@@ -189,7 +189,7 @@ async function obtenerTodosLosAlumnosAudit() {
 async function actualizarAlumno(id, datos, usuarioModificador) {
   const {
     nombre, apellido_paterno, apellido_materno,
-    dni, direccion, telefono, fecha_nacimiento, estado
+    tipo_documento,numero_documento, direccion, telefono, fecha_nacimiento, estado
   } = datos;
 
   try {
@@ -207,14 +207,14 @@ async function actualizarAlumno(id, datos, usuarioModificador) {
     const sqlUpdate = `
       UPDATE tb_alumno
       SET nombre = $1, apellido_paterno = $2, apellido_materno = $3,
-          dni = $4, direccion = $5, telefono = $6, fecha_nacimiento = $7,
-          estado = $8
-      WHERE id_alumno = $9
+          tipo_documento =$4, numero_documento = $5, direccion = $6, telefono = $7, fecha_nacimiento = $8,
+          estado = $9
+      WHERE id_alumno = $10
     `;
 
     await pool.query(sqlUpdate, [
       nombre, apellido_paterno, apellido_materno,
-      dni, direccion, telefono, fecha_nacimiento,
+      tipo_documento, numero_documento,direccion, telefono, fecha_nacimiento,
       estado, id
     ]);
 
@@ -226,8 +226,8 @@ async function actualizarAlumno(id, datos, usuarioModificador) {
       apellido_paterno_nuevo: apellido_paterno,
       apellido_materno_anterior: anterior.apellido_materno,
       apellido_materno_nuevo: apellido_materno,
-      dni_anterior: anterior.dni,
-      dni_nuevo: dni,
+      numero_documento_anterior: anterior.numero_documento,
+      numero_documento_nuevo: numero_documento,
       direccion_anterior: anterior.direccion,
       direccion_nuevo: direccion,
       telefono_anterior: anterior.telefono,
@@ -274,8 +274,8 @@ async function eliminarAlumno(id, usuarioModificador) {
       apellido_paterno_nuevo: anterior.apellido_paterno,
       apellido_materno_anterior: anterior.apellido_materno,
       apellido_materno_nuevo: anterior.apellido_materno,
-      dni_anterior: anterior.dni,
-      dni_nuevo: anterior.dni,
+      numero_documento_anterior: anterior.numero_documento,
+      numero_documento_nuevo: anterior.numero_documento,
       direccion_anterior: anterior.direccion,
       direccion_nuevo: anterior.direccion,
       telefono_anterior: anterior.telefono,
