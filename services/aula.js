bfdb17a57c432d3b457e8679a4269f67a4fa8f83
Ernesto <ernesto7816@gmail.com>
2025-06-27 22:26:15 -0500

    AULA.SERVICE4

--- services/aula.js
+++ services/aula.js
@@ -167,7 +167,7 @@ async function actualizarAula(id, datos, usuarioModificador) {
       ubicacion_nueva: ubicacion,
       estado_anterior: anterior.estado,
       estado_nuevo: estado,
-      observacion:observacion,
+      observacion:'ACTUALIZAR AULA',
       operacion: 'UPDATE',
       usuario: usuarioModificador.usuario
     });
